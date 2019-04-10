057ad6f8ef8e3bde1a9aad55d42ca756f7c34ed6
mao12312 <42489486+mao12312@users.noreply.github.com>
2019-04-10 14:52:32 +0900

    disable facebooklogin

--- resources/views/partials/socials.blade.php
+++ resources/views/partials/socials.blade.php
@@ -1,7 +1,7 @@
 <div class="row">
-    <div class="col-sm-6 mb-2">
-        {!! HTML::icon_link(route('social.redirect',['provider' => 'facebook']), 'fa fa-facebook', 'Facebook', array('class' => 'btn btn-block btn-social btn-facebook')) !!}
-    </div>
+    {{--<div class="col-sm-6 mb-2">--}}
+        {{--{!! HTML::icon_link(route('social.redirect',['provider' => 'facebook']), 'fa fa-facebook', 'Facebook', array('class' => 'btn btn-block btn-social btn-facebook')) !!}--}}
+    {{--</div>--}}
     {{--<div class="col-sm-6 mb-2">--}}
         {{--{!! HTML::icon_link(route('social.redirect',['provider' => 'twitter']), 'fa fa-twitter', 'Twitter', array('class' => 'btn btn-block btn-social btn-twitter')) !!}--}}
     {{--</div>--}}
